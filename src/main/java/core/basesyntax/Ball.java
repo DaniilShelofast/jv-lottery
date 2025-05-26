f764e962323bcc4ca3459b548edc2c2b4e23586c
Danyil <shelofast.daniil@gmail.com>
2025-05-26 15:50:55 +0300

    homework

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+public class Ball {
+
+    private Colors colors;
+    private int number;
+
+    public Ball(Colors colors, int number) {
+        this.colors = colors;
+        this.number = number;
+    }
+
+    public Ball() {
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "colors=" + colors +
+                ", number=" + number +
+                '}';
+    }
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(100));
+    }
+
+}
